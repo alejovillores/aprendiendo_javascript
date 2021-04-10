af11a53314d5ab96d395360ab58b94c5dfda452a
Alejo <alejovillores@gmail.com>
2021-04-09 22:22:05 -0300

    cadenas y arrays

---
+++ arrays.js
@@ -0,0 +1,10 @@
+//Metodos y manejos de arrays
+let lista = ['alejo', 'jorge', 'esteban', 'jose'];
+
+// pop() -> elimina el ultimo elemento del array
+let ultimoElemento = lista.pop(); //jose -> ahora el array es de 3 elementos
+
+//shift() ->elimina el primero, como pop
+let primerElemento = lista.shift(); // alejo -> ahora el array es de 2
+
+//
--- cadenas.js
+++ cadenas.js
@@ -1,43 +1,43 @@
 // Metodos y usos de cadenas en javascript
 
 //concat() -> + ;
-let cadena1 = "cadena 1";
-let cadena2 = " cadena 2";
-let cadenaConcatenada  = cadena1.concat(cadena2);
+let cadena1 = 'cadena 1';
+let cadena2 = ' cadena 2';
+let cadenaConcatenada = cadena1.concat(cadena2);
 
 // startsWith() -> boolean
-let boleano = cadena2.startsWith(" ");
+let boleano = cadena2.startsWith(' ');
 
 // endsWith() -> boolean
-let boleano2 = cadena1.endsWith("1");
+let boleano2 = cadena1.endsWith('1');
 
 // includes() -> boolean
-let cadenaLarga = "soy alejo villores";
-let esta = cadenaLarga.includes("alejo"); // True
+let cadenaLarga = 'soy alejo villores';
+let esta = cadenaLarga.includes('alejo'); // True
 
 // indexOf -> posicion de la primer letra, -1 si no está
-let posicion = cadenaLarga.indexOf("alejo"); // 4
+let posicion = cadenaLarga.indexOf('alejo'); // 4
 
 // lastIndexOf -> posicion de la ultima letra, -1 si no está
-let posicion = cadenaLarga.lastIndexOf("alejo"); 
+let posicion = cadenaLarga.lastIndexOf('alejo');
 
 //repeat -> n veces que quiera
-let cadenaRepetida = cadena1.repeat(2) // "cadena 1cadena 1"
+let cadenaRepetida = cadena1.repeat(2); // "cadena 1cadena 1"
 
 //split() -> como python
-let listaConPalabras = cadena1.split(" ");
+let listaConPalabras = cadena1.split(' ');
 
 //subtring() -> divide en cadenas chiquitas
-let cadenaChiquita = cadenaLarga.substring(0,2); // indexInicio, indexFin
+let cadenaChiquita = cadenaLarga.substring(0, 2); // indexInicio, indexFin
 
 //toUpperCase -> a mayusculas
 //toLowerCase -> a minusculas
-let nombre = "aLeJo";
+let nombre = 'aLeJo';
 let nombreMayuscula = nombre.toUpperCase();
 let nombreMinuscula = nombre.toLowerCase();
 
-// trim() -> elimina espacios
-let espacios = "    alejo   ";
+// trim()     -> elimina espacios
+//trimEnd()   -> solo los del final
+//trimStart() -> solo los del inicio
+let espacios = '    alejo   ';
 let sinEspacios = espacios.trim(); // "alejo"
-
-
